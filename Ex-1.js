5b162cfa3f448d177cad8ec36cd8afc3dea19b8f
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2025-03-13 06:52:09 +0530

    Add files via upload

---
+++ Ex-1.js
@@ -0,0 +1,17 @@
+alert('Good morning!');
+
+alert('My name');
+
+10 + 5
+
+20 - 5
+
+2 + 2 - 5
+
+document.body.innerHTML = 'Good morning!';
+
+10 + 8 + 20
+
+100 - (20 + 50) + 200
+
+document.body.innerHTML = '';
